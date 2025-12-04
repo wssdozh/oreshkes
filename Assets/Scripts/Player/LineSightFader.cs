e9845b33e2802269d55832fa0cddde0b81140328
moriiiiiiiiiiii12 <wssdozh@gmail.com>
2025-12-04 22:42:11 +0300

    add office bot

--- Assets/Scripts/Player/LineSightFader.cs
+++ Assets/Scripts/Player/LineSightFader.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LineOfSightFader : MonoBehaviour
+public class LineSightFader : MonoBehaviour
 {
     [SerializeField] private Transform _cameraTransform;
     [SerializeField] private Transform _target;
